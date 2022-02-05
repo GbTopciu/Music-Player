5531d3d363e979e11e1a6f8debd16a27cc70e80a
Gabriel Topciu <gb.topciu@gmail.com>
2022-02-05 09:51:50 +0000

    Added the player, songs .scss files. Completed the initial styling of the app

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import React from "react";
-import Player from './components/Player'
-import Song from './components/Song'
+//Import Styles
+import './styles/app.scss';
+//Import components
+import Player from './components/Player';
+import Song from './components/Song';
 
 function App() {
   return <div className="App">
